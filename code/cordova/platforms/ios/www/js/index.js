a3d6dfa80c6f4115ba6cbd949bd27b4cb662e1c0
shekit <as7790@nyu.edu>
2016-04-24 02:00:40 -0400

    fixed bug in cordova wrapper rendering

--- code/cordova/platforms/ios/www/js/index.js
+++ code/cordova/platforms/ios/www/js/index.js
@@ -1,51 +1,184 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+var peeqo = {
+    service: '12ab',
+    ssid: '34cd',
+    password: '45ef'
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+function stringToBytes(string){
+    var array = new Uint8Array(string.length);
+    for(var i=0, l=string.length; i<l;i++){
+        array[i] = string.charCodeAt(i);
     }
-};
+    return array.buffer;
+}
 
-app.initialize();
+
+(function(){
+
+    var peeqo_id = null;
+    var stop_ble_search = null;
+    var connected_via_ble = false;
+
+    var wrapper = $("wrapper")
+
+
+    var searchPeeqoTimer = function(){
+        stop_ble_search = setTimeout(function(){
+
+            ble.stopScan(function(){
+                console.log("Stopped scan - no devices found");
+            }, function() {
+                console.log("couldn't stop");
+            });
+                                    
+            render_not_found_peeqo();
+
+        }, 5000);
+ 
+    }
+
+    var clearSearchPeeqoTimer = function(){
+        clearTimeout(stop_ble_search);
+    }
+
+    Handlebars.registerPartial({
+        'empty_header': '<header class="bar bar-nav">'+
+                            'h1 class="title">Peeqo</h1>'+
+                        '</header>'
+    })
+
+    var find_peeqo_template = Handlebars.compile($("#find_peeqo").html());
+    var found_peeqo_template = Handlebars.compile($("#found_peeqo").html());
+    var not_found_peeqo_template = Handlebars.compile($("#not_found_peeqo").html());
+    var pairing_error_template = Handlebars.compile($("#pairing_error").html());
+    var wifi_template = Handlebars.compile($("#wifi").html())
+
+    function render_find_peeqo(){
+        wrapper.html(find_peeqo_template());
+        connected_via_ble = true;
+        ble.startScan([], onFoundPeeqo, onError);
+        searchPeeqoTimer();
+    }
+
+    function render_found_peeqo(device){
+        wrapper.html(found_peeqo_template(device));
+    }
+
+    function render_not_found_peeqo(){
+        wrapper.html(not_found_peeqo_template());
+    }
+
+    function render_pairing_error(){
+        wrapper.html(pairing_error_template());
+    }
+
+    function render_wifi(){
+        wrapper.html(wifi_template())
+    }
+
+    // start finding peeqo immediately
+    render_find_peeqo();
+
+    ///**** EVENTS ****////
+
+    document.addEventListener('deviceready', function(){
+
+        // ios7 status bar issue fix
+        StatusBar.overlaysWebView(false);
+        StatusBar.backgroundColorByHexString('#ffffff');
+        StatusBar.styleDefault();
+
+        FastClick.attach(document.body);
+
+        // override default HTML alert to look native
+        if(navigator.notification) {
+            window.alert = function(message) {
+                navigator.notification.alert(
+                    message,  //the message
+                    null,     // callback
+                    "Lumen",  // title
+                    'OK'      // button name
+                );
+            };
+        }
+
+    }, false)
+
+    $(document).on('click', '#connect', function(e){
+        e.preventDefault();
+
+        if($(e.target).attr('data-device-id')){
+            console.log("setting lumen to device id");
+            var device_id = e.target.dataset.deviceId;
+            peeqo_id = device_id;
+        } else {
+            console.log("no data attr")
+        }
+
+        $(".loading").css('display', 'block')
+        console.log(peeqo_id);
+        ble.connect(peeqo_id, pairedPeeqoSuccess, pairedPeeqoError)
+    })
+
+    $(document).on('click', '#find', function(e){
+        e.preventDefault();
+        render_find_peeqo();
+    })
+
+    $(document).on('click', '#reconnect', function(e){
+        e.preventDefault();
+        render_find_peeqo();
+    })
+
+    $(document).on('click', '#connect_to_wifi', function(e){
+        e.preventDefault();
+
+        var ssid = $("#wifi_ssid").val()
+        var pass = $("#wifi_pass").val()
+        
+
+        if(ssid && pass){
+            // write ssid
+            ble.write(peeqo_id, peeqo.service, peeqo.ssid, stringToBytes(ssid))
+
+            //write password
+            ble.write(peeqo_id, peeqo.service, peeqo.password, stringToBytes(pass))
+
+            $("#wifi_ssid").val("");
+            $("#wifi_pass").val("");
+        } else {
+            alert("Enter wifi details")
+        }
+        
+    })
+
+    function onFoundPeeqo(device){
+        if(device.name.match(/peeqo/i) && typeof(device) == 'object'){
+            ble.stopScan(function(){
+                console.log("Stopped scan, peeqo found")
+            }, function(){
+                console.log("couldnt stop scan")
+            })
+
+            clearSearchPeeqoTimer();
+
+            render_found_peeqo(device);
+        } else {
+            console.log("a device i dont care about")
+        }
+    }
+
+    function onError(msg){
+        alert("ERROR: "+msg)
+    }
+
+    function pairedPeeqoSuccess(){
+        render_wifi();
+    }
+
+    function pairedPeeqoError(){
+        render_pairing_error();
+    }
+
+}());
