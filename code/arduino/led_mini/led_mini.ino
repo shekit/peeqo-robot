09a84c287d9323cd4740e56deeb57e9cb177f33b
shekit <as7790@nyu.edu>
2016-05-01 13:16:22 -0400

    integrated i2c communication in led sketch

--- code/arduino/led_mini/led_mini.ino
+++ code/arduino/led_mini/led_mini.ino
@@ -3,27 +3,49 @@
 
 #define PIXEL_PIN 6
 #define PIXEL_COUNT 12
+#define RING_BRIGHTNESS 32
+
+#define I2C_ADDR 0x04
 
 Adafruit_NeoPixel ring = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
-unsigned long interval=50;
 unsigned long previousMillis=0;
+
+// WIPE ANIMATION
+unsigned long wipeInterval=50;
 int currentPixel = 0;
 
+// FADE ANIMATION
+int r = 0;
+int rspeed = 5;
+int g = 0;
+int gspeed = 2;
+
+// BLINK ANIMATION
+unsigned long blinkinterval = 300;
+
+// LED STATES
 boolean red = false;
 boolean green = false;
 boolean blue = false;
 boolean off = false;
+boolean fade = false;
+boolean blinky = false;
+
+boolean lights = false;
+boolean rainbow = false;
+
 
 void setup() {
   // put your setup code here, to run once:
-  Wire.begin(0x04);
+  Wire.begin(I2C_ADDR);
   Wire.onReceive(receiveEvent);
   
   Serial.begin(9600);
   
   ring.begin();
-  ring.show();
+  ring.setBrightness(RING_BRIGHTNESS);
+  switchOff();
 }
 
 void loop() {
@@ -40,24 +62,64 @@ void loop() {
            green = false;
            blue = false;
            off = false;
+           blinky = false;
+           rainbow = false;
+           fade = false;
            break;
         case 'g':
            green = true;
            red = false;
            blue = false;
            off = false;
+           blinky = false;
+           rainbow = false;
+           fade = false;
            break;
         case 'b':
            blue = true;
            green = false;
            red = false;
            off = false;
+           blinky = false;
+           rainbow = false;
+           fade = false;
            break; 
         case 'o':
            red = false;
            green = false;
            blue = false;
            off = true;
+           blinky = false;
+           rainbow = false;
+           fade = false;
+           break;
+        case 't':
+           red = false;
+           green = false;
+           blue = false;
+           off = false;
+           blinky = true;
+           rainbow = false;
+           fade = false;
+           break;
+        case 'p':
+           red = false;
+           green = false;
+           blue = false;
+           off = false;
+           blinky = false;
+           rainbow = true;
+           fade = false;
+           break;
+        case 'f':
+           red = false;
+           green = false;
+           blue = false;
+           off = false;
+           blinky = false;
+           rainbow = false;
+           fade = true;
+           break;
         default:
           break;
      }
@@ -65,29 +127,70 @@ void loop() {
   }
   
   if(red){
-    if(millis()-previousMillis >= interval){
+    if(millis()-previousMillis >= wipeInterval){
        previousMillis = millis();
        colorWipeMillis(ring.Color(255,0,0)); 
     }
   }
   
   if(green){
-    if(millis()-previousMillis >= interval){
+    if(millis()-previousMillis >= wipeInterval){
        previousMillis = millis();
        colorWipeMillis(ring.Color(0,255,0)); 
     }
   }
   
   if(blue){
-    if(millis()-previousMillis >= interval){
+    if(millis()-previousMillis >= wipeInterval){
        previousMillis = millis();
        colorWipeMillis(ring.Color(0,0,255)); 
+    }
+  }
+  
+  if(blinky){
+      if(millis()-previousMillis >= blinkinterval){
+         previousMillis = millis();
+         if(lights == false){
+           lights = true;
+         } else {
+            lights = false; 
+         }
+         
+         lightsBlink(lights);
+       }
+  }
+  
+  if(rainbow){
+    if(millis()-previousMillis >= 20){
+       previousMillis = millis();
+       fade2();
+    }
+  }
+  
+  if(fade){
+    if(millis()-previousMillis >= 20){
+       previousMillis = millis();
+       fade2();
     }
   }
   
   if(off){
     switchOff();
   }
+}
+
+void lightsBlink(boolean f){
+   if(f==true){
+      for(uint16_t i=0; i<ring.numPixels(); i++) {
+        ring.setPixelColor(i, ring.Color(255,0,0));
+        ring.show();
+      }  
+   } else {
+       for(uint16_t i=0; i<ring.numPixels(); i++) {
+        ring.setPixelColor(i, ring.Color(0,0,0));
+        ring.show();
+      }
+   }
 }
 
 void colorWipeMillis(uint32_t c){
@@ -99,7 +202,6 @@ void colorWipeMillis(uint32_t c){
       for(uint16_t i=0; i<ring.numPixels(); i++) {
         ring.setPixelColor(i, ring.Color(0,0,0));
         ring.show();
-      
       }
    }
 }
@@ -107,93 +209,73 @@ void colorWipeMillis(uint32_t c){
 void switchOff(){
   currentPixel = 0;
   for(uint16_t i=0; i<ring.numPixels(); i++) {
-        ring.setPixelColor(i, ring.Color(0,0,0));
-         ring.show();
-      }
+     ring.setPixelColor(i, ring.Color(0,0,0));
+     ring.show();
+  }
 }
 
-void printit(char c){
-   
-}
 
 void receiveEvent(int howMany){
    while(Wire.available()>0){
       int c = Wire.read();
       
       if(c!=1){
-         runLights(c); 
+         //runLights(c); 
+         setLightState(c);
       }
    } 
 }
 
-void runLights(char val){
+void setLightState(int val){
+   switch(val){
+      case 2:
+        red = true;
+        green = false;
+        blue = false;
+        off = false;
+        blinky = false;
+        rainbow = false;
+        fade = false;
+        break;
+      case 3:
+        red = false;
+        green = false;
+        blue = false;
+        off = false;
+        blinky = true;
+        rainbow = false;
+        fade = false;
+        break;
+      case 4:
+        red = false;
+        green = false;
+        blue = false;
+        off = true;
+        blinky = false;
+        rainbow = false;
+        fade = false;
+        break;
+      default:
+        Serial.println("rubbish");
+        break;
+   } 
+}
+
+void fade2(){
    
-  switch (val){
-     case 'q':
-       colorWipe(ring.Color(255, 0, 0), 50);
-       break;
-     case 'w':
-       colorWipe(ring.Color(0, 255, 0), 50);
-       break;
-     case 'e':
-       colorWipe(ring.Color(0, 0, 255), 50);
-       break;
-     case 'r':
-       theaterChase(ring.Color(127,   0,   0), 50);
-       break;
-     case 't':
-       theaterChaseRainbow(50);
-       break;
-     default:
-       colorWipe(ring.Color(0, 0, 0), 50);
-       break;
-  } 
-  
-}
-
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<ring.numPixels(); i++) {
-      ring.setPixelColor(i, c);
+   for(int i=0; i<ring.numPixels();i++){
+      ring.setPixelColor(i,ring.Color(r,g,0));
       ring.show();
-      delay(wait);
-  }
-}
-
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < ring.numPixels(); i=i+3) {
-        ring.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      ring.show();
-     
-      delay(wait);
-     
-      for (int i=0; i < ring.numPixels(); i=i+3) {
-        ring.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-        for (int i=0; i < ring.numPixels(); i=i+3) {
-          ring.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-        }
-        ring.show();
-       
-        delay(wait);
-       
-        for (int i=0; i < ring.numPixels(); i=i+3) {
-          ring.setPixelColor(i+q, 0);        //turn every third pixel off
-        }
-    }
-  }
+   } 
+   //r+=5;
+   
+   r+=rspeed;
+   g+=gspeed;
+   
+   if(r >= 255 || r<=0){
+      rspeed *= -1; 
+      gspeed *= -1;
+   }
 }
 
 // Input a value 0 to 255 to get a color value.
