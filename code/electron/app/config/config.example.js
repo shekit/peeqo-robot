32ce3359f460204a94b4b7e1e47da0c3fbf748bd
shekit <as7790@nyu.edu>
2017-02-22 19:16:50 -0500

    added sample config.js file

---
+++ code/electron/app/config/config.example.js
@@ -0,0 +1,13 @@
+module.exports = {
+	server: '', // api.ai key
+	token: '',// Use Client access token there (see agent keys).
+	sessionId: '',
+	giphyKey: 'dc6zaTOxFJmzC', // public key
+	spotifyKey: '', // spotify api key
+	ssid: '', //wifi ssid
+	sandboxPass: '', //wifi pass
+	timezone: '', //for
+	hueUser: '', // philips hue user id
+	hueIp: '', //philips hue hub ip
+	peeqoServer: '' // server you set up for socket connections
+}
