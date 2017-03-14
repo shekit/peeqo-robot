59c521446b7d0bfdc707e0e43c01f5fc1809c144
shekit <as7790@nyu.edu>
2017-03-14 01:38:44 -0400

    included updated module versions

--- electron/listen.js
+++ electron/listen.js
@@ -2,6 +2,7 @@
 
 // Load in smart mirror config
 const path = require('path')
+const os = require('os')
 const config = require(path.join(__dirname,"app/config/config.js"))
 if(!config || !config.speech || !config.speech.keyFilename || !config.speech.model || !config.language){
   throw "Configuration Error! See: https://docs.smart-mirror.io/docs/configure_the_mirror.html#speech";
@@ -14,19 +15,13 @@ const speech = require('@google-cloud/speech')({
   keyFilename: path.join(__dirname, 'app/config/', config.speech.keyFilename)
 })
 
-let hotwords = []
-
-if(typeof config.speech.model == 'string'){
-  hotwords.push({ file: path.join(__dirname, 'app/config/', config.speech.model), hotword: config.speech.keyword, sensitivity:  config.speech.sensitivity || '0.5'})
-} else {
-  return
-  for(let i =0; i < config.speech.model.length; i++){
-    hotwords.push({ file: config.speech.model[i], hotword: config.speech.keyword[i], sensitivity:  config.speech.sensitivity || '0.5'})
-  }
-}
-
+const hotwords = [{ file: path.join(__dirname, 'app/config/', config.speech.model), hotword: config.speech.keyword, sensitivity:  config.speech.sensitivity || '0.5'}]
 const language = config.language
-const sonus = Sonus.init({ hotwords, language }, speech)
+
+
+const recordProgram = (os.arch() == 'arm') ? "arecord" : "rec"
+
+const sonus = Sonus.init({ hotwords, language, recordProgram}, speech)
 
 // Start Recognition
 Sonus.start(sonus)
